6f44b4a345961f297c27da7816fc01ecbfcd0d04
shubha agrawal <shubh.cap@gmail.com>
2023-03-01 17:54:46 -0500

    day2 final commit

--- index.js
+++ index.js
@@ -1,25 +1,87 @@
+import { words } from "./words.js";
+let guessedwords = [[]];
+let availableSpace = 1;
 
-
-document.addEventListener("DOMContentLoaded",function(){
 
     
+let word = words[Math.floor(Math.random() * words.length)];
+console.log(word);
+console.log(words[0]);
+document.addEventListener("DOMContentLoaded",function(){
+
     createGameBoard();
     createKeyBoard();
     const key = document.querySelectorAll('button');
-    // const square = document.querySelector('.square');
+    
     key.forEach(element => {
         element.addEventListener('click',function(e){
-           
-            let letter = e.target;
-            console.log(letter.textContent);
-            let square = document.getElementById('1');
-            console.log(square);
-            square.textContent = letter.textContent;
+            
+            let letter = e.target.textContent;
+            if (letter === "del") {
+                deleteLetter();
+                return;
+            }else if (letter === "enter") {
+                checkGuessWord();
+                return;
+            } else 
+            updateGuessedWords(letter);            
         })
     });
-   
 })
 
+function deleteLetter(){
+    const currentWordArr = getCurrentWordArray();
+    const removedLetter = currentWordArr.pop();
+
+    guessedwords[guessedwords.length-1] = currentWordArr;
+    const lastLetterEl = document.getElementById(availableSpace-1);
+    lastLetterEl.textContent = '';
+    availableSpace -= 1;
+}
+
+function checkGuessWord(){
+    const currentWordArr = getCurrentWordArray();
+    if(currentWordArr.length !== 5){
+    window.alert("Word should be of 5 letters")
+    return;
+    }
+
+    const currentWord = currentWordArr.join('');
+    if(currentWord === word){
+        window.alert("congratulations")
+        
+    }
+    if(!words.includes(currentWord)){
+    
+        alert('Word entered is not in list');
+        return;
+    }
+    
+
+    if(guessedwords.length === 6){
+        window.alert(`You have no more guesses left, Word is ${word}`);
+    }
+    guessedwords.push([]);
+    console.log(guessedwords);
+
+
+}
+
+function getCurrentWordArray(){
+    const numberOfGuessedWords = guessedwords.length;
+    return guessedwords[numberOfGuessedWords - 1];
+}
+function updateGuessedWords(letter){
+    const currentWordArr = getCurrentWordArray();
+
+    if(currentWordArr && currentWordArr.length < 5){
+        currentWordArr.push(letter);
+        const availableSpaceElement = document.getElementById(availableSpace);
+        availableSpaceElement.textContent = letter;
+        availableSpace += 1;
+    }
+        
+}
 
 // To create board which has 5*6 squares to fill in letters
 function createGameBoard(){
@@ -38,7 +100,7 @@ function createKeyBoard(){
     const keyboard = document.getElementById('keyboard');
     let keyboardArray = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','del','z','x','c','v','b','n','m','enter'];
           
-        for (i=0;i<keyboardArray.length;i++){
+        for (let i=0;i<keyboardArray.length;i++){
             const key = document.createElement('button');
             key.classList.add('key');
             //Assigning id same as keyvalue

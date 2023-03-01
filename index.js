911c9843ff8ce07ef8027120572acb4b58db2f61
shubha agrawal <shubh.cap@gmail.com>
2023-03-01 12:31:48 -0500

    day2starting commit

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ document.addEventListener("DOMContentLoaded",function(){
     
     createGameBoard();
     createKeyBoard();
+    const key = document.querySelectorAll('button');
+    // const square = document.querySelector('.square');
+    key.forEach(element => {
+        element.addEventListener('click',function(e){
+           
+            let letter = e.target;
+            console.log(letter.textContent);
+            let square = document.querySelector('.squaresRow');
+            console.log(square);
+            square.firstChild.textContent = letter.textContent;
+            
+        })
+
+    });
+   
 })
 
 
@@ -19,6 +34,7 @@ function createGameBoard(){
         for(j=0;j<5;j++){
             const square = document.createElement('div');
             square.classList.add('square');
+
             row.appendChild(square);
         }
         gameboard.appendChild(row);
@@ -27,21 +43,13 @@ function createGameBoard(){
 }
 
 function createKeyBoard(){
-
     const keyboard = document.getElementById('keyboard');
-    let keyboardArray = ['q','w','e','r','t','y','u','i','o','p'];
-    console.log(keyboardArray.length);
-    for (let i=0;i<3;i++){
-        const keyboardRow = document.createElement('div');
-        keyboardRow.classList.add('keyboardRow');
-        for (let j=0;j<keyboardArray.length;j++){
+    let keyboardArray = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','del','z','x','c','v','b','n','m','enter'];
+          
+        for (i=0;i<keyboardArray.length;i++){
             const key = document.createElement('button');
             key.classList.add('key');
-            key.textContent = keyboardArray[j];
-            keyboardRow.appendChild(key);
+            key.textContent = keyboardArray[i];
+            keyboard.appendChild(key);
         }
-        keyboard.appendChild(keyboardRow);
-    
     }
-    
-}

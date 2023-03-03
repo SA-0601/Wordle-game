829b3e0aed512bb59d64ce269d406dba29f08656
shubha agrawal <shubh.cap@gmail.com>
2023-03-02 22:26:35 -0500

    day3 final commit

--- index.js
+++ index.js
@@ -32,45 +32,76 @@ document.addEventListener("DOMContentLoaded",function(){
 function deleteLetter(){
     const currentWordArr = getCurrentWordArray();
     const removedLetter = currentWordArr.pop();
-
+    
     guessedwords[guessedwords.length-1] = currentWordArr;
     const lastLetterEl = document.getElementById(availableSpace-1);
+
+    // if we want to delete when nothing is there on row
+    if(lastLetterEl === null){
+        window.alert('No more letters to delete');
+        return;
+    }else
+
     lastLetterEl.textContent = '';
     availableSpace -= 1;
 }
 
 function checkGuessWord(){
     const currentWordArr = getCurrentWordArray();
+    console.log(currentWordArr);
     if(currentWordArr.length !== 5){
     window.alert("Word should be of 5 letters")
     return;
     }
 
     const currentWord = currentWordArr.join('');
+    console.log(currentWord);
     if(currentWord === word){
         window.alert("congratulations")
-        
     }
+
     if(!words.includes(currentWord)){
     
         alert('Word entered is not in list');
         return;
+    }
+
+    // for adding colors to letters based on their presence
+    let index=availableSpace-5;
+    
+    for(let i=0;i<5;i++){
+        const letterOnGrid = document.getElementById(index);
+        console.log(letterOnGrid);
+        let char = letterOnGrid.textContent;
+        console.log(word[i]);
+        if(char == word[i]){
+            
+            letterOnGrid.classList.add('green');
+            
+        }else if (word.includes(char)){
+                letterOnGrid.classList.add('yellow');
+              } else
+                letterOnGrid.classList.add('gray');
+        index += 1;
+        console.log(char);
+        
     }
     
 
     if(guessedwords.length === 6){
         window.alert(`You have no more guesses left, Word is ${word}`);
     }
+
     guessedwords.push([]);
     console.log(guessedwords);
-
-
 }
+
 
 function getCurrentWordArray(){
     const numberOfGuessedWords = guessedwords.length;
     return guessedwords[numberOfGuessedWords - 1];
 }
+
 function updateGuessedWords(letter){
     const currentWordArr = getCurrentWordArray();
 
